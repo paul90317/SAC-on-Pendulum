85d25ef7c6f4f6144a2e9c096a1781f6838a0ca5
Paul Wu <paul2309825@gmail.com>
2025-05-12 20:00:25 +0800

    class network

--- train_pendulum.py
+++ train_pendulum.py
@@ -2,7 +2,7 @@ import time
 import gymnasium as gym
 from gymnasium.wrappers import TimeLimit
 from replay_buffer import ReplayBuffer, Transition
-from params_pool import ParamsPool
+from trainer import Trainer
 
 import argparse
 
@@ -15,7 +15,7 @@ args = parser.parse_args()
 
 env = gym.make('Pendulum-v1')
 buf = ReplayBuffer(capacity=int(1e6))
-param = ParamsPool(
+trainer = Trainer(
     input_dim=env.observation_space.shape[0],
     action_dim=env.action_space.shape[0]
 )
@@ -38,7 +38,7 @@ for e in range(num_episodes):
         # getting the tuple (s, a, r, s', done)
         # ==================================================
 
-        action = param.act(obs)
+        action = trainer.step(obs)
         next_obs, reward, ter, tru, _ = env.step(action)
         # no need to keep track of max time-steps, because the environment
         # is wrapped with TimeLimit automatically (timeout after 1000 steps)
@@ -57,7 +57,7 @@ for e in range(num_episodes):
         # ==================================================
 
         if buf.ready_for(batch_size):
-            param.update_networks(buf.sample(batch_size))
+            trainer.update_networks(buf.sample(batch_size))
             total_updates += 1
 
         # ==================================================
@@ -78,7 +78,7 @@ for e in range(num_episodes):
 
     print(f'Episode {e:4.0f} | Return {total_reward:9.3f} | Updates {total_updates:4.0f} | Remaining time {round(time_remaining / 3600, 2):5.2f} hours')
 
-param.save_actor(
+trainer.save_actor(
     save_dir='results/trained_policies_pth/',
     filename=f'{args.run_id}.pth'
 )
--- trainer.py
+++ trainer.py
@@ -4,9 +4,23 @@ import torch
 import torch.nn as nn
 from torch import optim
 import torch.nn.functional as F
-from torch.distributions import Normal, Independent
+from torch.distributions import Normal
+from torch import Tensor
 
 from replay_buffer import Batch
+
+device = torch.device('cuda')
+
+def tensor(x):
+    return torch.tensor(x, device=device, dtype=torch.float32)
+
+class NetWork(nn.Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+    def post_init(self, lr):
+        self.optimizer = optim.Adam(self.parameters(), lr=lr)
+        self.to(device)
 
 def get_net(
         num_in:int,
@@ -36,59 +50,57 @@ def get_net(
 
     return nn.Sequential(*layers)
 
-class NormalPolicyNet(nn.Module):
+class Actor(NetWork):
 
     """Outputs a distribution with parameters learnable by gradient descent."""
 
     def __init__(self, input_dim, action_dim):
-        super(NormalPolicyNet, self).__init__()
+        super(Actor, self).__init__()
         self.shared_net   = get_net(num_in=input_dim, num_out=64, final_activation=nn.ReLU())
         self.means_net    = nn.Linear(64, action_dim)
         self.log_stds_net = nn.Linear(64, action_dim)
+        self.post_init(1e-3)
+        
 
-    def forward(self, states: torch.tensor):
-
-        out = self.shared_net(states)
+    def forward(self, obs: Tensor):
+        out = self.shared_net(obs)
         means, log_stds = self.means_net(out), self.log_stds_net(out)
 
-        # the gradient of computing log_stds first and then using torch.exp
-        # is much more well-behaved then computing stds directly using nn.Softplus()
-        # ref: https://github.com/openai/spinningup/blob/master/spinup/algos/pytorch/sac/core.py#L26
+        stds = torch.exp(torch.clamp(log_stds, -20, 2))
+        dist = Normal(loc=means, scale=stds)
 
-        LOG_STD_MAX = 2
-        LOG_STD_MIN = -20
+        u = dist.rsample()
+        a = 2 * torch.tanh(u)
+        logp = dist.log_prob(u) - 2 * (np.log(2) - u - F.softplus(-2 * u)) - np.log(2)
+        return a, logp
+    
+    def act(self, obs) -> tuple[Tensor, Tensor]:
+        return self(obs)
 
-        stds = torch.exp(torch.clamp(log_stds, LOG_STD_MIN, LOG_STD_MAX))
-
-        return Independent(Normal(loc=means, scale=stds), reinterpreted_batch_ndims=1)
-
-class QNet(nn.Module):
+class QNet(NetWork):
 
     """Has little quirks; just a wrapper so that I don't need to call concat many times"""
 
     def __init__(self, input_dim, action_dim):
         super(QNet, self).__init__()
         self.net = get_net(num_in=input_dim+action_dim, num_out=1, final_activation=None)
+        self.post_init(1e-3)
 
-    def forward(self, states: torch.tensor, actions: torch.tensor):
+    def forward(self, states: Tensor, actions: Tensor):
         return self.net(torch.cat([states, actions], dim=1))
 
-class ParamsPool:
-
+class Trainer:
     def __init__(self, input_dim, action_dim):
 
-        self.Normal   = NormalPolicyNet(input_dim=input_dim, action_dim=action_dim)
-        self.Normal_optimizer = optim.Adam(self.Normal.parameters(), lr=1e-3)
+        self.actor   = Actor(input_dim=input_dim, action_dim=action_dim)
 
         self.Q1       = QNet(input_dim=input_dim, action_dim=action_dim)
         self.Q1_targ  = QNet(input_dim=input_dim, action_dim=action_dim)
         self.Q1_targ.load_state_dict(self.Q1.state_dict())
-        self.Q1_optimizer = optim.Adam(self.Q1.parameters(), lr=1e-3)
 
         self.Q2       = QNet(input_dim=input_dim, action_dim=action_dim)
         self.Q2_targ  = QNet(input_dim=input_dim, action_dim=action_dim)
         self.Q2_targ.load_state_dict(self.Q2.state_dict())
-        self.Q2_optimizer = optim.Adam(self.Q2.parameters(), lr=1e-3)
 
         self.gamma = 0.99
         self.alpha = 0.1
@@ -98,22 +110,11 @@ class ParamsPool:
     # Helper methods (it is generally not my style of using helper methods but here they improve readability)
     # ==================================================================================================================
 
-    def min_i_12(self, a: torch.tensor, b: torch.tensor) -> torch.tensor:
-        return torch.min(a, b)
-
-    def sample_action_and_compute_log_pi(self, state: torch.tensor, use_reparametrization_trick: bool) -> tuple:
-        mu_given_s = self.Normal(state)  # in paper, mu represents the normal distribution
-        # in paper, u represents the un-squashed action; nu stands for next u's
-        # actually, we can just use reparametrization trick in both Step 12 and 14, but it might be good to separate
-        # the two cases for pedagogical purposes, i.e., using reparametrization trick is a must in Step 14
-        u = mu_given_s.rsample() if use_reparametrization_trick else mu_given_s.sample()
-        a = 2 * torch.tanh(u)
-        # the following line of code is not numerically stable:
-        # log_pi_a_given_s = mu_given_s.log_prob(u) - torch.sum(torch.log(1 - torch.tanh(u) ** 2), dim=1)
-        # ref: https://github.com/vitchyr/rlkit/blob/0073d73235d7b4265cd9abe1683b30786d863ffe/rlkit/torch/distributions.py#L358
-        # ref: https://github.com/tensorflow/probability/blob/master/tensorflow_probability/python/bijectors/tanh.py#L73
-        log_pi_a_given_s = mu_given_s.log_prob(u) - 2 * (np.log(2) - u - F.softplus(-2 * u)) - np.log(2)
-        return a, log_pi_a_given_s
+    def step(self, obs: np.ndarray) -> tuple:
+        with torch.no_grad():
+            obs = tensor(obs).unsqueeze(0)
+            act, _ = self.actor.act(obs)
+            return act[0].cpu().numpy()
 
     def clip_gradient(self, net: nn.Module) -> None:
         for param in net.parameters():
@@ -128,36 +129,39 @@ class ParamsPool:
     # ==================================================================================================================
 
     def update_networks(self, b: Batch) -> None:
-
+        b_s = b.s.to(device)
+        b_ns = b.ns.to(device)
+        b_d = b.d.to(device)
+        b_r = b.r.to(device)
+        b_a = b.a.to(device)
         # ========================================
         # Step 12: calculating targets
         # ========================================
 
         with torch.no_grad():
-
-            na, log_pi_na_given_ns = self.sample_action_and_compute_log_pi(b.ns, use_reparametrization_trick=False)
-            targets = b.r + self.gamma * (1 - b.d) * \
-                      (self.min_i_12(self.Q1_targ(b.ns, na), self.Q2_targ(b.ns, na)) - self.alpha * log_pi_na_given_ns)
+            na, logp = self.actor.act(b_ns)
+            targets = b_r + self.gamma * (1 - b_d) * \
+                      (torch.min(self.Q1_targ(b_ns, na), self.Q2_targ(b_ns, na)) - self.alpha * logp)
 
         # ========================================
         # Step 13: learning the Q functions
         # ========================================
 
-        Q1_predictions = self.Q1(b.s, b.a)
+        Q1_predictions = self.Q1(b_s, b_a)
         Q1_loss = torch.mean((Q1_predictions - targets) ** 2)
 
-        self.Q1_optimizer.zero_grad()
+        self.Q1.optimizer.zero_grad()
         Q1_loss.backward()
         self.clip_gradient(net=self.Q1)
-        self.Q1_optimizer.step()
+        self.Q1.optimizer.step()
 
-        Q2_predictions = self.Q2(b.s, b.a)
+        Q2_predictions = self.Q2(b_s, b_a)
         Q2_loss = torch.mean((Q2_predictions - targets) ** 2)
 
-        self.Q2_optimizer.zero_grad()
+        self.Q2.optimizer.zero_grad()
         Q2_loss.backward()
         self.clip_gradient(net=self.Q2)
-        self.Q2_optimizer.step()
+        self.Q2.optimizer.step()
 
         # ========================================
         # Step 14: learning the policy
@@ -168,13 +172,13 @@ class ParamsPool:
         for param in self.Q2.parameters():
             param.requires_grad = False
 
-        a, log_pi_a_given_s = self.sample_action_and_compute_log_pi(b.s, use_reparametrization_trick=True)
-        policy_loss = - torch.mean(self.min_i_12(self.Q1(b.s, a), self.Q2(b.s, a)) - self.alpha * log_pi_a_given_s)
+        a, logp = self.actor.act(b_s)
+        policy_loss = - torch.mean(torch.min(self.Q1(b_s, a), self.Q2(b_s, a)) - self.alpha * logp)
 
-        self.Normal_optimizer.zero_grad()
+        self.actor.optimizer.zero_grad()
         policy_loss.backward()
-        self.clip_gradient(net=self.Normal)
-        self.Normal_optimizer.step()
+        self.clip_gradient(net=self.actor)
+        self.actor.optimizer.step()
 
         for param in self.Q1.parameters():
             param.requires_grad = True
@@ -189,17 +193,12 @@ class ParamsPool:
             self.polyak_update(old_net=self.Q1_targ, new_net=self.Q1)
             self.polyak_update(old_net=self.Q2_targ, new_net=self.Q2)
 
-    def act(self, state: np.array) -> np.array:
-        state = torch.tensor(state).unsqueeze(0).float()
-        action, _ = self.sample_action_and_compute_log_pi(state, use_reparametrization_trick=False)
-        return action.numpy()[0]  # no need to detach first because we are not using the reparametrization trick
-
     def save_actor(self, save_dir: str, filename: str) -> None:
         os.makedirs(save_dir, exist_ok=True)
-        torch.save(self.Normal.state_dict(), os.path.join(save_dir, filename))
+        torch.save(self.actor.state_dict(), os.path.join(save_dir, filename))
 
     def load_actor(self, save_dir: str, filename: str) -> None:
-        self.Normal.load_state_dict(torch.load(os.path.join(save_dir, filename)))
+        self.actor.load_state_dict(torch.load(os.path.join(save_dir, filename)))
 
 
 
